4dc45e290e0347428a00ae1d7af0ca4e76dea125
Hannes Leitner <hannes.leitner@me.com>
2015-03-22 21:50:59 +0100

    added projects to mitglieder page

---
+++ public_html/js/controllers/MitgliedCtrl.js
@@ -0,0 +1,39 @@
+kazoosh.controller('MitgliedCtrl', ['CONF', '$scope', '$state', 'ContentService', '$q', function(CONF, $scope, $state, ContentService, $q) {
+
+
+	loadProject = function(content){
+
+		content.projects.forEach(function(path, i){
+			requests.push(ContentService.getContent(path, function(){}));
+		});
+
+		
+		$q.all(requests)
+			.then(
+				function(data){
+					$scope.content.projects = data;
+				},
+				function(data){
+					console.error('FAIL');
+				}
+			);
+	}
+
+	//get featured items
+	var requests = [];
+
+
+	if($scope.content == undefined){
+
+		$scope.$watch('content', function() {
+			
+			if($scope.content != undefined){
+				loadProject($scope.content);
+			}
+		});
+	}
+	else{
+		loadProject($scope.content);
+	}
+	
+}]);
