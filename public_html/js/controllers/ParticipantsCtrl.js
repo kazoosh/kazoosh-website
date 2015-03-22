fe13360f71d4dd0c4a9e419166f2b2a3deebd54e
Hannes Leitner <hannes.leitner@me.com>
2015-03-22 23:03:33 +0100

    added participants to project page

---
+++ public_html/js/controllers/ParticipantsCtrl.js
@@ -0,0 +1,50 @@
+kazoosh.controller('ParticipantsCtrl', ['CONF', '$scope', '$state', 'ContentService', '$q', function(CONF, $scope, $state, ContentService, $q) {
+
+
+	loadParticipants = function(content){
+
+		ContentService.getContent('root/mitglieder').then(
+
+			function(content){
+
+				var requests = [];
+				content.subpages.forEach(function(subpage, i){
+					requests.push(ContentService.getContent(subpage.path, function(){}));
+				});
+
+				$q.all(requests)
+					.then(
+						function(participants){
+							$scope.participants = [];
+							//filter participants by path of current content
+							participants.forEach(function(participant){
+								participant.projects.forEach(function(project){
+									if(project == $scope.content.path){
+										$scope.participants.push(participant);
+									}
+								});
+							});
+						},
+						function(data){
+							console.error('FAIL');
+						}
+					);
+			},
+			function(){
+				$state.go('404');
+			}
+		);
+	}
+
+	if($scope.content == undefined){
+		$scope.$watch('content', function() {
+			if($scope.content != undefined){
+				loadParticipants($scope.content);
+			}
+		});
+	}
+	else{
+		loadParticipants($scope.content);
+	}
+	
+}]);
