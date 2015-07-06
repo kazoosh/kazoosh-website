be9c48164a6fd37617fccddc5b77581e3d7783b3
Brian Eschrich <hi@brinoausrino.com>
2015-07-06 17:44:55 +0200

    link protection contact page

--- public_html/js/non-angular/sticky-header.js
+++ public_html/js/non-angular/sticky-header.js
@@ -1,14 +1,3 @@
-//set header position on startpage
-/*$(document).ready(function(){
- 	var str = window.location.href; 
- 	console.log(str.indexOf('home'));
-	if (str.indexOf('home') != -1) {
-		$('nav').css('top',$(window).height()- $('nav').outerHeight()); 
-	};
-}); */
-
-
-//sticky header
 $(window).scroll(function() {
     if ($(this).scrollTop() > $('header').height()-$('nav').outerHeight()){  
         $('nav').addClass("sticky");
