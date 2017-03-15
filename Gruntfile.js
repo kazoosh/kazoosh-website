2e64f573f184f442ea1695f23b5a829448354996
Hannes Leitner <hannes.leitner@me.com>
2017-03-14 22:31:09 -0500

    chnaged jshint conf. use jshint conf in grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,108 +1,116 @@
-module.exports = function(grunt) {
-	
-	// Project configuration.
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		CONF: grunt.file.readJSON('config.json'),
-		sass: {
-			dist: {
-				options: {
-					style: 'expanded'
+(function () {
+	'use strict';
+
+	/*global module:false*/
+	module.exports = function(grunt) {
+
+		// Project configuration.
+		grunt.initConfig({
+			pkg: grunt.file.readJSON('package.json'),
+			CONF: grunt.file.readJSON('config.json'),
+			sass: {
+				dist: {
+					options: {
+						style: 'expanded'
+					},
+					files: {
+						'public_html/css/style.css': 'public_html/sass/style.scss'
+					}
+				}
+			},
+			watch: {
+				content: {
+					files: ['<%= CONF.contentSourceDirectory %>/**'],
+					tasks: ['content'],
+					options: {
+						spawn: false,
+					}
 				},
-				files: {
-					'public_html/css/style.css': 'public_html/sass/style.scss'
-				}
-			}
-		},
-		watch: {
-			content: {
-				files: ['<%= CONF.contentSourceDirectory %>/**'],
-				tasks: ['content'],
-				options: {
-					spawn: false,
+				images: {
+					files: ['<%= CONF.imagesSourceDirectory %>/**'],
+					tasks: ['clean:images', 'copy:images'],//use images watch event for performance issues
+					options: {
+						spawn: false,
+					}
+				},
+				css: {
+					files: ['public_html/sass/*.scss'],
+					tasks: ['sass'],
+				},
+				jshint: {
+					files: ['<%= jshint.files %>'],
+					tasks: ['jshint'],
+				},
+			},
+			shell: {
+				mdToJson: {
+					command: function (sourceDirectory, destinationDirectory) {
+						var script = 'python script/mdToJson.py '+sourceDirectory+' '+destinationDirectory;
+						return script;
+					}
 				}
 			},
-			images: {
-				files: ['<%= CONF.imagesSourceDirectory %>/**'],
-				tasks: ['clean:images', 'copy:images'],//use images watch event for performance issues
-				options: {
-					spawn: false,
-				}
+			clean: {
+				images: ['<%= CONF.imagesDestinationDirectory %>'],
 			},
-			css: {
-				files: ['public_html/sass/*.scss'],
-				tasks: ['sass']
+			copy: {
+				images: {
+					cwd: '<%= CONF.imagesSourceDirectory %>',
+					src: '**',
+					dest: '<%= CONF.imagesDestinationDirectory %>',
+					expand: true,
+				},
 			},
 			jshint: {
-				files: ['<%= jshint.files %>'],
-				tasks: ['jshint']
-			},
-		},
-		shell: {
-			mdToJson: {
-				command: function (sourceDirectory, destinationDirectory) {
-					var script = 'python script/mdToJson.py '+sourceDirectory+' '+destinationDirectory;
-					return script;
-				}
+				files: ['Gruntfile.js', 'public_html/js/**/*.js'],
+				options: {
+						jshintrc: true,
+				},
 			}
-		},
-		clean: {
-			images: ['<%= CONF.imagesDestinationDirectory %>'],
-		},
-		copy: {
-			images: {
-				cwd: '<%= CONF.imagesSourceDirectory %>',
-				src: '**',
-				dest: '<%= CONF.imagesDestinationDirectory %>',
-				expand: true,
-			},
-		},
-		jshint: {
-			files: ['Gruntfile.js', 'public_html/js/**/*.js']
+		});
+		
+		//merge local config
+		if(grunt.file.exists('config.local.json')){
+			grunt.config.merge({CONF: grunt.file.readJSON('config.local.json')});
 		}
-	});
-	
-	//merge local config
-	if(grunt.file.exists('config.local.json')){
-		grunt.config.merge({CONF: grunt.file.readJSON('config.local.json')});
-	}
 
 
-	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.event.on('watch', function(action, filepath, target) {
+			
+			grunt.log.write("watch: action:"+action+", filepath: "+filepath+", target: "+target);
+			
+			//TODO: ony 'deleted' action seems to be working on server
+			/*
+
+			if (target === 'images') {
+				var pathArray = filepath.split("/");
+				var imgFilePath = pathArray.slice(1).join("/");//path without image folder
+				imgFilePath = grunt.config.get('CONF.imagesDestinationDirectory') +'/'+imgFilePath;
+
+				if (action === 'deleted') {
+					grunt.log.write("delete image "+imgFilePath);
+					grunt.file.delete(imgFilePath);
+				}
+				else if(action === 'added' || action === 'changed' || action === 'renamed'){
+					grunt.log.write("copy image from "+filepath+" to "+imgFilePath);
+					grunt.file.copy(filepath, imgFilePath)
+				}
+			}*/
+		});
+
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-shell');
+		grunt.loadNpmTasks('grunt-contrib-clean');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-sass');
+		grunt.loadNpmTasks('grunt-contrib-jshint');
+
 		
-		grunt.log.write("watch: action:"+action+", filepath: "+filepath+", target: "+target);
-		
-		//TODO: ony 'deleted' action seems to be working on server
-		/*
+		grunt.registerTask('content', ['shell:mdToJson:<%= CONF.contentSourceDirectory %>:<%= CONF.contentDestinationDirectory %>']);
+		grunt.registerTask('images', ['clean:images', 'copy:images']);
 
-		if (target === 'images') {
-			var pathArray = filepath.split("/");
-			var imgFilePath = pathArray.slice(1).join("/");//path without image folder
-			imgFilePath = grunt.config.get('CONF.imagesDestinationDirectory') +'/'+imgFilePath;
+		grunt.registerTask('observe', ['sass', 'content', 'images', 'watch']);
 
-			if (action === 'deleted') {
-				grunt.log.write("delete image "+imgFilePath);
-				grunt.file.delete(imgFilePath);
-			}
-			else if(action === 'added' || action === 'changed' || action === 'renamed'){
-				grunt.log.write("copy image from "+filepath+" to "+imgFilePath);
-				grunt.file.copy(filepath, imgFilePath)
-			}
-		}*/
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-
-	
-	grunt.registerTask('content', ['shell:mdToJson:<%= CONF.contentSourceDirectory %>:<%= CONF.contentDestinationDirectory %>']);
-	grunt.registerTask('images', ['clean:images', 'copy:images']);
-
-	grunt.registerTask('observe', ['sass', 'content', 'images', 'watch']);
-
-	grunt.registerTask('observe-contents', ['content', 'images', 'watch']);
-};
+		grunt.registerTask('observe-contents', ['content', 'images', 'watch']);
+	};
+}());
